00dea6fe9032c248840eebe9dcb0c1e1b2c9a01f
BenoitGhirardini63 <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-27 11:13:44 +0200

    modif

--- verify_files.py
+++ verify_files.py
@@ -9,13 +9,14 @@ import os
 class verify():
     def verify_file(self, files):
         chemin = os.getenv('HOME')
-
+        st.write(files)
+        
         #files = r'C:\Users\F269167\OneDrive - MFP Michelin\Projets en cours\Suivi formations\Base Excel-Access\InstallV3\Fichiers\EFF Cfe à Fin fév 2022 sans coeff.xlsx'
         try:
             read_file = pd.read_excel (files)
             
             st.write(read_file)
-            
+
         #Remplace toutes les entêtes qui contiennent des ' ' par des '_'
             i = 0
             ii = 0
