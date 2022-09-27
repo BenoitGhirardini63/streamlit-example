8b3fdf82ce779d8342c59e7fafd846fbf28d2061
BenoitGhirardini63 <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-27 10:53:23 +0200

    modif

--- verify_files.py
+++ verify_files.py
@@ -12,7 +12,6 @@ class verify():
 
         chemin = f"C:/Users/{os.environ.get('USERNAME')}/Documents"
 
-        st.write(chemin)
         #files = r'C:\Users\F269167\OneDrive - MFP Michelin\Projets en cours\Suivi formations\Base Excel-Access\InstallV3\Fichiers\EFF Cfe à Fin fév 2022 sans coeff.xlsx'
         try:
             read_file = pd.read_excel (files)
