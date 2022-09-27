d9aa16a7a0979ea711b545d7e9d57a01e844fd38
BenoitGhirardini63 <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-27 13:47:46 +0200

    modif

--- verify_files.py
+++ verify_files.py
@@ -3,13 +3,14 @@ import streamlit as st
 import pandas as pd
 from numpy import *
 import os
-
+import sys
 
 
 class verify():
     def verify_file(self, files):
         chemin = os.getenv('HOME')
         st.write(files)
+        st.write(sys.argv)
         st.write(os.environ.get("ENV_NAME"))
 
         #files = r'C:\Users\F269167\OneDrive - MFP Michelin\Projets en cours\Suivi formations\Base Excel-Access\InstallV3\Fichiers\EFF Cfe à Fin fév 2022 sans coeff.xlsx'
