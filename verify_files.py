b02f2f53d8280d4d48581ae951e41fed9b6ed146
BenoitGhirardini63 <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-27 13:45:04 +0200

    modif

--- verify_files.py
+++ verify_files.py
@@ -10,7 +10,7 @@ class verify():
     def verify_file(self, files):
         chemin = os.getenv('HOME')
         st.write(files)
-        st.write(os.getenv["USERPROFILE"])
+        st.write(os.getenv["USERNAME"])
 
         #files = r'C:\Users\F269167\OneDrive - MFP Michelin\Projets en cours\Suivi formations\Base Excel-Access\InstallV3\Fichiers\EFF Cfe à Fin fév 2022 sans coeff.xlsx'
         try:
