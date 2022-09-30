23cb4e1f2392f70ca8269f8b131145cf506de6dc
Benoit.ghirardini <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-30 15:58:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,4 @@ my_system = platform.uname()
  
 
 st.title("Bonjour")
-import subprocess
- 
-# traverse the info
-Id = subprocess.check_output(['systeminfo']).decode('utf-8').split('\n')
-new = []
- 
-# arrange the string into clear info
-for item in Id:
-    new.append(str(item.split("\r")[:-1]))
-for i in new:
-    st.write(i[2:-2])
+st.write(os.path.expanduser('~'))
