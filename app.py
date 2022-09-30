a6a98fd04d27bd1cc05702e320eb1ae48f46b16f
Benoit.ghirardini <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-30 15:51:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import os
 
+import platform
+ 
+my_system = platform.uname()
+ 
 
 st.title("Bonjour")
-st.write(os.path.expanduser(dirname))
+st.write(f"System: {my_system.system}")
+st.write(f"Node Name: {my_system.node}")
+st.write(f"Release: {my_system.release}")
+st.write(f"Version: {my_system.version}")
+st.write(f"Machine: {my_system.machine}")
+st.write(f"Processor: {my_system.processor}")))
