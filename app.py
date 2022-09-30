ef44b3174d0e8582918d09e72b47a7343a953651
Benoit.ghirardini <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-09-30 15:52:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ st.write(f"Release: {my_system.release}")
 st.write(f"Version: {my_system.version}")
 st.write(f"Machine: {my_system.machine}")
 st.write(f"Processor: {my_system.processor}")
+
+import wmi
+ 
+c = wmi.WMI()   
+my_system = c.Win32_ComputerSystem()[0]
+ 
+st.write(f"Manufacturer: {my_system.Manufacturer}")
+st.write(f"Model: {my_system. Model}")
+st.write(f"Name: {my_system.Name}")
+st.write(f"NumberOfProcessors: {my_system.NumberOfProcessors}")
+st.write(f"SystemType: {my_system.SystemType}")
+st.write(f"SystemFamily: {my_system.SystemFamily}")
