54b8be9251799e5a0662b7b4206efd233418f45f
Benoit.ghirardini <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-10-03 09:25:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
  
 import platform
 
-st.write(st.secret["DB_USERNAME"])
+st.write(st.secrets["DB_USERNAME"])
 st.write(st.secrets["DB_PATH"])
  
-os.environ["db_username"] == st.secret["DB_USERNAME"]
+os.environ["db_username"] == st.secrets["DB_USERNAME"]
 st.write(os.environ["db_username"])
 
 st.title("Bonjour")
