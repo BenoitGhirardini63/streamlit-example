ed56586320196a47f32948fed3ce73eb89210d54
Benoit.ghirardini <94698193+BenoitGhirardini63@users.noreply.github.com>
2022-10-03 09:39:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ import os
  
 import platform
 
-st.write(st.secrets["DB_USERNAME"])
 st.write(st.secrets["DB_PATH"])
- 
 
-iD = "F222222" 
-st.secrets["DB_USERNAME"] = iD 
 st.title("Bonjour")
 
-st.write(st.secrets["DB_USERNAME"])
